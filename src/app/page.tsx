98cfa91dd9e57c52a2c55c220dd5cef062adbf18
Lana Sanad <lana.sanad@gmail.com>
2023-12-03 19:02:54 +1100

    Add card grid with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,30 @@ import Button from "@mui/joy/Button";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
 import Box from "@mui/joy/Box";
+import Grid from "@mui/joy/Grid";
 import Textarea from "@mui/joy/Textarea";
 import Add from "@mui/icons-material/Add";
 
 export default function Home() {
-  return (
-    <Box padding="20px">
+  const gratitudeList = [
+    "For myself!",
+    "I am grateful for the chirping panther",
+    "I am grateful for the chirping mimi and her cute cat ears",
+    "I am grateful for the chirping elephant, I am grateful for the chirping elephant.",
+    "I am grateful for the chirping lion",
+    "I am grateful for the chirping monkey",
+  ];
 
-      <Box display="flex" justifyContent="space-between" marginBottom="50px">
+  return (
+    <Box padding="30px">
+      <Box display="flex" justifyContent="space-between" marginBottom="40px">
         <Box>
-          <Typography level="h1">Gratitudes</Typography>
-          <Typography level="title-lg">A community board for sharing everyday gratitudes ✨</Typography>
+          <Typography level="h1" mb={1}>
+            Gratitudes
+          </Typography>
+          <Typography level="title-lg">
+            A community board for sharing everyday gratitudes ✨
+          </Typography>
         </Box>
         <Box alignSelf="flex-start">
           <Button variant="soft" startDecorator={<Add />}>
@@ -22,37 +35,26 @@ export default function Home() {
           </Button>
         </Box>
       </Box>
-
-      <Card
-        variant="outlined"
-        sx={{
-          minHeight: "280px",
-          width: 320,
-          backgroundColor: "#e6e6fa",
-          borderColor: "#000",
-          border: "2px solid",
-        }}
-      >
-        <Typography level="h2" fontSize="lg" textColor="#000">
-          My Gratitude
-        </Typography>
-
-        <CardContent
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            background: "#b0c4de",
-            border: "1px solid",
-            borderColor: "#000",
-          }}
-        >
-          <Textarea
-            size="sm"
-            placeholder="I am grateful for.."
-            variant="plain"
-          />
-        </CardContent>
-      </Card>
+      <Box display="flex" justifyContent="center">
+        <Grid container spacing={3}>
+          {gratitudeList.map((post, index) => (
+            <Grid>
+              <Card
+                variant="solid"
+                sx={{
+                  height: "70px",
+                  width: "280px",
+                  backgroundColor: "#e6e6fa",
+                }}
+              >
+                <CardContent>
+                  <Typography level="body-md">{post}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Box>
   );
 }
